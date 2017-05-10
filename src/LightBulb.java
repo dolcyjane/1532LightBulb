6ca0cbbd233afbe72edd99bcdeb6646ddbade4b4
Dolcy <fernandesdolcy@gmail.com>
2017-05-10 11:50:35 -0700

    Hello1

---
+++ src/LightBulb.java
@@ -0,0 +1,20 @@
+
+public class LightBulb {
+	int watts;
+	String type;
+	String color;
+int getBulbWattage(int watts)
+{
+	return watts;
+}
+
+String getBulbType(String type)
+{
+	return type;
+}
+String getBulbColor(String color)
+{
+	return color;
+}
+
+}
